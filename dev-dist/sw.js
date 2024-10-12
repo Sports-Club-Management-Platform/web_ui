ab7b4b94a84ee45b21e63f00b36130aabf78cc19
MrLoydHD <hf.correia@ua.pt>
2024-10-12 09:17:16 +0100

    fix: remove package-lock.json file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hakqdt1tvtg"
+    "revision": "0.osuc21tumhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
