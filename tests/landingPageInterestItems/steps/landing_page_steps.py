cb272d78733da70173823c9e27fee03c28b03487
Migas77 <miguel.belchior@ua.pt>
2024-10-12 21:28:58 +0100

    SCRUM-9 | feat: added tests for UI - lacks redirection link

---
+++ tests/landingPageInterestItems/steps/landing_page_steps.py
@@ -0,0 +1,38 @@
+from behave import given, when, then, step
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+pages = {
+    "login_sign_up": "http://localhost:5173/login-sign-up",
+}
+
+
+@given("a new user,")
+def given_a_new_user(context):
+    context.driver = webdriver.Chrome()
+
+
+@when("they visit the landing page")
+def they_visit_the_landing_page(context):
+    context.driver.get("http://localhost:5173/")
+
+
+@step("select “{button_text}“ button,")
+def select_button_with_text(context, button_text):
+    login_button = context.driver.find_element(By.XPATH, f"//button[text()='{button_text}']")
+    login_button.click()
+
+
+@then("the user should be redirected to the {page_name} page.")
+def the_user_should_be_redirected_to_the_login_sign_up_page(context, page_name):
+    current_url = context.driver.current_url
+    expected_url = pages[page_name]
+    assert expected_url is not None
+    assert current_url == expected_url
+
+
+@then('there should exist a "{button_text}" button')
+def there_should_exist_a_button_named(context, button_text):
+    button = context.driver.find_element(By.XPATH, f"//button[text()='{button_text}']")
+    assert button is not None
