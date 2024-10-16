cda04ff0925212c807f65be2927db4e18f013554
Your Name <joaquimvr15@gmail.com>
2024-10-16 01:02:42 +0100

    fix user register test

--- tests/userRegistrationInterestItems/steps/registration_page_steps.py
+++ tests/userRegistrationInterestItems/steps/registration_page_steps.py
@@ -27,13 +27,13 @@ def select_the_sign_up_button(context):
 def provide_valid_information(context):
     wait = WebDriverWait(context.driver, 10)
 
-    username_input = wait.until(EC.presence_of_element_located((By.NAME, "username")))
-    first_name_input = context.driver.find_element(By.NAME, "requiredAttributes[given_name]")
-    last_name_input = context.driver.find_element(By.NAME, "requiredAttributes[family_name]")
-    email_input = context.driver.find_element(By.NAME, "requiredAttributes[email]")
-    password_input = context.driver.find_element(By.NAME, "password")
+    username_input = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/div[1]/input")))
+    first_name_input = context.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/div[2]/div/input")
+    last_name_input = context.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/div[3]/div/input")
+    email_input = context.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/div[4]/div/input")
+    password_input = context.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/input[2]")
 
-    username_input.send_keys("usertest")
+    username_input.send_keys("test")
     first_name_input.send_keys("User")
     last_name_input.send_keys("Test")
     email_input.send_keys("teste@gmail.com")
@@ -45,14 +45,14 @@ def provide_valid_information(context):
     assert email_input is not None
     assert password_input is not None
 
-    sign_up_button = context.driver.find_element(By.XPATH, "//button[text()='Sign up']")
+    sign_up_button = context.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/button")
     sign_up_button.click()
 
 @then("they should be able to create an account.")
 def they_should_be_able_to_create_an_account(context):
     wait = WebDriverWait(context.driver, 10)
-    confirm_account_header = wait.until(EC.presence_of_element_located((By.XPATH, "//h3[text()='Confirm your account']")))
+    already_exists_header = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[3]/div[2]/div/form/p[1]")))
 
-    assert confirm_account_header is not None
+    assert already_exists_header is not None
     
 
