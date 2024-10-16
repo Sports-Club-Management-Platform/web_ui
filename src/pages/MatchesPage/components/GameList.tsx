7165125dfca60e50e2a836352792f70272afd0b6
MrLoydHD <hf.correia@ua.pt>
2024-10-16 02:08:28 +0100

    SCRUM:-28: fix: fix duplicated code

--- src/pages/MatchesPage/components/GameList.tsx
+++ src/pages/MatchesPage/components/GameList.tsx
@@ -8,6 +8,7 @@ import { startOfWeek, endOfWeek, startOfMonth, endOfMonth, parseISO, isWithinInt
 import { Button } from "@/components/ui/button"
 import GameCard from './GameCard'
 import NextGame from './NextGame'
+import FilterControls from "./FilterControls"
 
 const GameList = ({ jogos, proximoJogo, getBilheteStatus, dataAtual }) => {
   const [filtro, setFiltro] = useState("futuros")
@@ -55,73 +56,14 @@ const GameList = ({ jogos, proximoJogo, getBilheteStatus, dataAtual }) => {
   return (
     <div className="container pt-36 mx-auto p-4 space-y-6">
       {proximoJogo && <NextGame proximoJogo={proximoJogo} />}
-
-      <div className="flex  flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
-        <div className="flex flex-wrap justify-center md:justify-start gap-2">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button variant="outline" className="w-[280px] justify-start text-left font-normal">
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {dataFiltro ? format(dataFiltro, "PPP") : <span>Selecionar data</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={dataFiltro}
-                onSelect={(date) => {
-                  setDataFiltro(date)
-                  setFiltro(date ? "semana" : "todos")
-                }}
-                initialFocus
-              />
-              {dataFiltro && (
-                <div className="flex justify-center space-x-2 p-2 border-t">
-                  <Button
-                    variant={filtro === "semana" ? "default" : "outline"}
-                    onClick={() => setFiltro("semana")}
-                    size="sm"
-                  >
-                    Semana
-                  </Button>
-                  <Button
-                    variant={filtro === "mes" ? "default" : "outline"}
-                    onClick={() => setFiltro("mes")}
-                    size="sm"
-                  >
-                    Mês
-                  </Button>
-                </div>
-              )}
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div className="flex space-x-2 w-full md:w-auto">
-          <div className="relative flex-grow md:w-64">
-            <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              placeholder="Pesquisar por data ou equipe"
-              value={pesquisa}
-              onChange={(e) => setPesquisa(e.target.value)}
-              className="pl-8"
-            />
-          </div>
-          <Select value={filtro} onValueChange={setFiltro}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Selecione o período" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="futuros">Jogos Futuros</SelectItem>
-              <SelectItem value="todos">Todos os jogos</SelectItem>
-              <SelectItem value="1">1º Trimestre</SelectItem>
-              <SelectItem value="2">2º Trimestre</SelectItem>
-              <SelectItem value="3">3º Trimestre</SelectItem>
-              <SelectItem value="4">4º Trimestre</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-
+        <FilterControls
+            filtro={filtro}
+            setFiltro={setFiltro}
+            pesquisa={pesquisa}
+            setPesquisa={setPesquisa}
+            dataFiltro={dataFiltro}
+            setDataFiltro={setDataFiltro}
+        />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {jogosFiltrados.map((jogo, index) => (
           <GameCard
