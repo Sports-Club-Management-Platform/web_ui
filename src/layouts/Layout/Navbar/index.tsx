eb3a7b9b6e2d1888e60a96757b61af9f31c166c9
MrLoydHD <hf.correia@ua.pt>
2024-10-17 12:45:00 +0100

    SCRUM-36: feature: first version of ticket purchas page + minor fixes in Navbar

--- src/layouts/Layout/Navbar/index.tsx
+++ src/layouts/Layout/Navbar/index.tsx
@@ -13,14 +13,15 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { useTheme } from "@/components/theme-provider";
+import { Home, Users, Calendar, Menu } from "lucide-react";
 
 export default function Navbar() {
   const [scrollY, setScrollY] = useState(0);
   const [isOpen, setIsOpen] = useState(false)
-  const { token, givenName, familyName, logout : zustandLogout} = useUserStore();
+  const { token, givenName, familyName, logout: zustandLogout } = useUserStore();
   const { theme } = useTheme()
 
-  // scroll effect on the symbol
   useEffect(() => {
     const handleScroll = () => {
       setScrollY(window.scrollY);
@@ -34,7 +35,7 @@ export default function Navbar() {
   }, []);
 
   const maxScroll = 100;
-  const scrollFactor = Math.min(scrollY / maxScroll, 1); // Normaliza o scroll entre 0 e 1
+  const scrollFactor = Math.min(scrollY / maxScroll, 1);
 
   const logout = async () => {
     const response = await UserService.logout();
@@ -59,8 +60,8 @@ export default function Navbar() {
   };
   
   const logoSrc = theme === "special" 
-  ? "/src/assets/CSC-SemFundo-Special.png"
-  : "/src/assets/CSC-SemFundo.png"
+    ? "/src/assets/CSC-SemFundo-Special.png"
+    : "/src/assets/CSC-SemFundo.png"
   
   const NavItems = ({ inSheet = false }) => (
     <>
@@ -87,62 +88,64 @@ export default function Navbar() {
 
   return (
     <nav className={`fixed w-full z-10 transition-all duration-300 ease-linear ${scrollFactor > 0 ? 'bg-gradient-to-b from-background via-background dark:via-neutral-800 to-transparent' : ''}`}>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between items-center transition-all duration-300 ease-linear h-28">
-              <div className="hidden md:flex items-center">
-                <NavItems />
-              </div>
-              <div className="md:hidden">
-                <Sheet open={isOpen} onOpenChange={setIsOpen}>
-                  <SheetTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                      <Menu className="h-6 w-6" />
-                      <span className="sr-only">Toggle menu</span>
-                    </Button>
-                  </SheetTrigger>
-                  <SheetContent side="left" className="w-[300px] sm:w-[400px]">
-                    <SheetHeader>
-                      <SheetTitle>Menu</SheetTitle>
-                    </SheetHeader>
-                    <div className="flex flex-col space-y-4 mt-4">
-                      <NavItems inSheet={true} />
-                    </div>
-                  </SheetContent>
-                </Sheet>
-              </div>
-              <div className="flex-shrink-0 flex items-center transition-all duration-500">
-                <img
-                  src={logoSrc}
-                  alt="Candelária Sport Clube Logo"
-                  className={`transition-all duration-300 ease-linear ${scrollFactor ? 'h-12 sm:h-16 md:h-20' : 'h-16 sm:h-20 md:h-24'}`} 
-                />
-              </div>
-              <div className="flex items-center">
-                { token ? (
-                  <div className="flex items-center">
-                    <Avatar className="mr-2">
-                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <div className="hidden md:block text-primary mr-2 text-lg dark:text-white font-bold">
-                      {givenName} {familyName}
-                    </div>
-                    <Button onClick={handleLogout} variant="outline" className="mr-2">Logout</Button>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center transition-all duration-300 ease-linear h-28">
+          <div className="flex-1 flex items-center justify-start">
+            <div className="hidden md:flex items-center">
+              <NavItems />
+            </div>
+            <div className="md:hidden">
+              <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                <SheetTrigger asChild>
+                  <Button variant="ghost" size="icon">
+                    <Menu className="h-6 w-6" />
+                    <span className="sr-only">Toggle menu</span>
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+                  <SheetHeader>
+                    <SheetTitle>Menu</SheetTitle>
+                  </SheetHeader>
+                  <div className="flex flex-col space-y-4 mt-4">
+                    <NavItems inSheet={true} />
                   </div>
-                ) : (
-                  <div>
-                    <Link to={import.meta.env.VITE_LOGIN_SIGN_UP}>
-                      <Button variant="outline" className="mr-2">Login</Button>
-                    </Link>
-                    <Link to={import.meta.env.VITE_LOGIN_SIGN_UP}>
-                      <Button className="mr-2">Register</Button>
-                    </Link>
-                  </div>
-                )}
-                <ModeToggle />
-              </div>
+                </SheetContent>
+              </Sheet>
             </div>
           </div>
-        </nav>
-      )
-    }
+          <div className="flex-1 flex justify-center">
+            <img
+              src={logoSrc}
+              alt="Candelária Sport Clube Logo"
+              className={`transition-all duration-300 ease-linear ${scrollFactor ? 'h-12 sm:h-16 md:h-20' : 'h-16 sm:h-20 md:h-24'}`} 
+            />
+          </div>
+          <div className="flex-1 flex items-center justify-end">
+            {token ? (
+              <div className="flex items-center">
+                <Avatar className="mr-2">
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div className="hidden md:block text-primary mr-2 text-lg dark:text-white font-bold">
+                  {givenName} {familyName}
+                </div>
+                <Button onClick={handleLogout} variant="outline" className="mr-2">Logout</Button>
+              </div>
+            ) : (
+              <div>
+                <Link to={import.meta.env.VITE_LOGIN_SIGN_UP}>
+                  <Button variant="outline" className="mr-2">Login</Button>
+                </Link>
+                <Link to={import.meta.env.VITE_LOGIN_SIGN_UP}>
+                  <Button className="mr-2">Register</Button>
+                </Link>
+              </div>
+            )}
+            <ModeToggle />
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
