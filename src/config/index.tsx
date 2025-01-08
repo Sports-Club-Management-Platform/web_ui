418c82e684622a1035e5240e27fb35deea037c39
André Oliveira <andreaoliveira@ua.pt>
2025-01-08 22:11:32 +0000

    Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -5,30 +5,30 @@ const scheme = {
   HTTPS: "https://",
 };
 
-if (import.meta.env.PROD) {
+if (import.meta.env.VITE_PROD) {
   HOST = import.meta.env.VITE_DOMAIN;
   BASE_URL_USER = `${scheme.HTTPS}${HOST}/users/v1`;
   BASE_URL_TICKET = `${scheme.HTTPS}${HOST}/tickets/v1`;
   BASE_URL_GAME = `${scheme.HTTPS}${HOST}/games/v1`;
   BASE_URL_PAYMENTS = `${scheme.HTTPS}${HOST}/payments/v1`;
 } else {
-    HOST = 'localhost';
-    BASE_URL_USER = `${scheme.HTTP}${HOST}:8000`;
-    BASE_URL_TICKET = `${scheme.HTTP}${HOST}:8001`;
-    BASE_URL_GAME = `${scheme.HTTP}${HOST}:8002`;
-    BASE_URL_PAYMENTS = `${scheme.HTTP}${HOST}:8003`;
+  HOST = "localhost";
+  BASE_URL_USER = `${scheme.HTTP}${HOST}:8000`;
+  BASE_URL_TICKET = `${scheme.HTTP}${HOST}:8001`;
+  BASE_URL_GAME = `${scheme.HTTP}${HOST}:8002`;
+  BASE_URL_PAYMENTS = `${scheme.HTTP}${HOST}:8003`;
 }
 
 const config = {
-    PRODUCTION: import.meta.env.PROD,
-    HOST,
-    BASE_URL_USER,
-    API_USER_URL: `${BASE_URL_USER}/auth`,
-    API_GAME_URL: `${BASE_URL_GAME}/games`,
-    API_CLUBS_URL: `${BASE_URL_GAME}/clubs`,
-    API_PAVILIONS_URL: `${BASE_URL_GAME}/pavilions`,
-    API_TICKETS_URL: `${BASE_URL_TICKET}/tickets`,
-    API_PAYMENTS_URL: `${BASE_URL_PAYMENTS}`,
+  PRODUCTION: import.meta.env.PROD,
+  HOST,
+  BASE_URL_USER,
+  API_USER_URL: `${BASE_URL_USER}/auth`,
+  API_GAME_URL: `${BASE_URL_GAME}/games`,
+  API_CLUBS_URL: `${BASE_URL_GAME}/clubs`,
+  API_PAVILIONS_URL: `${BASE_URL_GAME}/pavilions`,
+  API_TICKETS_URL: `${BASE_URL_TICKET}/tickets`,
+  API_PAYMENTS_URL: `${BASE_URL_PAYMENTS}`,
 };
 
 export default config;
