d7ef0215616d2cb5e6a7c3385eae283af1c83ab7
André Oliveira <andreaoliveira@ua.pt>
2025-01-09 01:52:19 +0000

    url fixes

--- src/services/Client/ClubService.tsx
+++ src/services/Client/ClubService.tsx
@@ -5,21 +5,21 @@ import { ClubPost } from "@/lib/types";
 const client = createClient(config.API_CLUBS_URL);
 
 const ClubService = {
-    async getClubs() {
-        return client.get("/");
-    },
-    async getClub(id: string) {
-        return client.get(`/${id}`);
-    },
-    async createClub(data: ClubPost) {
-        return client.post("/", data);
-    },
-    async updateClub(id: string, data: ClubPost) {
-        return client.put(`/${id}`, data);
-    },
-    async deleteClub(id: string) {
-        return client.delete(`/${id}`);
-    },
+  async getClubs() {
+    return client.get("");
+  },
+  async getClub(id: string) {
+    return client.get(`/${id}`);
+  },
+  async createClub(data: ClubPost) {
+    return client.post("", data);
+  },
+  async updateClub(id: string, data: ClubPost) {
+    return client.put(`/${id}`, data);
+  },
+  async deleteClub(id: string) {
+    return client.delete(`/${id}`);
+  },
 };
 
 export { ClubService };
